795fa19076785c2703a1d803e6092e39047c948d
Mark Artherton <mark.artherton@gmail.com>
2017-02-22 17:41:36 +0000

    full reinstall

--- db/migrate/20160410121904_alterstringtointegerinissues.rb
+++ db/migrate/20160410121904_alterstringtointegerinissues.rb
@@ -1,6 +1,6 @@
 class Alterstringtointegerinissues < ActiveRecord::Migration
   def change
-  	change_column :issues, :category, 'integer USING CAST(column_name AS integer)'
-  	change_column :issues, :subcategory, 'integer USING CAST(column_name AS integer)'
+  	change_column :issues, :category, 'integer USING CAST(category AS integer)'
+  	change_column :issues, :subcategory, 'integer USING CAST(subcategory AS integer)'
   end
 end
